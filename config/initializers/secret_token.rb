18975423c7fe7d3ebdac1c0be10a6f9489b77bc3
Kidane Weldemariam <kidane.yemane@gmail.com>
2014-01-12 18:54:23 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heb::Application.config.secret_key_base = '3c3ad84b2a5a20620424e71f10e9a6970413e171ff24118645880581b169f5b5bfda22b7ee56c571a1405f25e5a212f0dab5f824f3e469fea2e3b91c8c794b5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Heb::Application.config.session_store :cookie_store, key: '_heb_session'
